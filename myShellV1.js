e442b804e73b46dd1d3be031eb3d446f8b21230f
anushri23p <162800663+anushri23p@users.noreply.github.com>
2024-12-20 09:27:15 +0530

    some changes done

--- myShellV1.js
+++ myShellV1.js
@@ -2,16 +2,19 @@ let currentDir = "~";
 const promptMsg = "anushriphadatare@Anushris-AnuBook-Pro ";
 const location = ["~"];
 const directory = [];
+const file = [];
+
 const echo = function (arg) {
   return arg.join("");
 };
 
 const cd = function (command, arg) {
   if (directory.includes(arg.join(""))) {
-    location = location.length === 0 ? location.push("~") : location.push(arg.join(" "));
+    location.length === 0 ? location.push("~") : location.push(arg.join(" "));
     currentDir = location.at(-1);
     return;
   }
+
   if (arg.join(" ") === "..") {
     location.pop();
     currentDir = location.length === 0 ? "/" : location.at(-1);
@@ -25,12 +28,17 @@ const mkdir = function (arg) {
   directory.push(arg.join(""));
 };
 
+const touch = function (arg) {
+  file.push(arg.join(""));
+};
+
 const executeCommand = function (commandAndArgs) {
   const [command, ...arg] = commandAndArgs.split(" ");
   switch (command) {
     case "echo": return echo(arg);
     case "cd": return cd(command, arg);
     case "mkdir": return mkdir(arg);
+    case "touch": return touch(arg);
     default: return "zsh: command not found :" + command;
   }
 
@@ -44,5 +52,3 @@ while (true) {
   }
 
 }
-
-//hgshd;
